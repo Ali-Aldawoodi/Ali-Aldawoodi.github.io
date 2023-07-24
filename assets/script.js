38f9733ff74b3b91cdc5c13764091fed4deba802
Ali Aldawoodi <124013342+Ali-Aldawoodi@users.noreply.github.com>
2023-07-23 21:07:47 -0600

    Added function for question 1 as well as defined variable for the question and answer

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,7 @@
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
 // I need to make the start button start the page through event listerners. I then need this to sync to a timer starting as well.(event listener. timer interval)
+        // maybe to get to the next page i have to create something to where i hide the previoius item and show the current. data attributes.
 
 // WHEN I answer a question
 // THEN I am presented with another question
@@ -29,16 +30,15 @@
 
 //variables
 let timer = document.querySelector(".timer");
-let start = document.querySelector(".start");
-let q1 = document.querySelector(".question1");
-
+let startButton = document.querySelector(".start");
+let questionContainerEl = document.querySelector(".question-container");
 
 //functions
-start.addEventListener("click", function () {
-    countdown();
-});
+start.addEventListener("click", startQuiz)
 
-function countdown() {
+function startQuiz() {
+    start.classList.add('hide');
+    questionContainerEl.classList.remove('hide');
     let timeLeft = 59;
     let totalTimer = setInterval(function () {
         if (timeLeft > 0) {
@@ -49,8 +49,34 @@ function countdown() {
             clearInterval(totalTimer);
         }
 
-    }, 1000);
+    }, 1000);   
 }
+
+// would i have to create an array of the choices then do a conditional where if the correct answer is chosen then
+
+let q1 = {
+        question: "How many primitive datatypes are there?",
+        choices: ['1', '2', '3', '4'],
+        answer: '4',
+    };
+
+    function present1(q1) {
+         let question1Div = document.getElementById('question1');
+        question1Div.textContent = q1.question;
+        
+        let q1Choices = document.querySelector(".choices1");
+
+        q1Choices.forEach(function(element, index){
+            element.textContent = q1.choices[index];
+        });
+
+      
+    };
+
+    console.log(q1.choices)
+   
+    present1(q1);
+
     //special functions
     
 
