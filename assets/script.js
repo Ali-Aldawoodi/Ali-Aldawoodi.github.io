2fac5b43657a6a14fa8b9586d3168485a3188363
Ali Aldawoodi <124013342+Ali-Aldawoodi@users.noreply.github.com>
2023-11-24 15:24:44 -0700

    Answer results are now displayed once a button is selected. Timer now stops at the end of answering all questions. the play again button now restarts the quiz

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,9 @@ var words = document.querySelector(".words-fill");
 var isWin = false;
 var timerCount = document.querySelector(".timer-countdown")
 var lose = document.querySelector(".lose-game")
-var winCounter= 0;
-var loseCounter= 0;
+let resultElement = document.getElementById('answer-result')
+var winCounter = 0;
+var loseCounter = 0;
 
 var timerReset;
 var timerElement;
@@ -12,58 +13,58 @@ var timerElement;
 
 
 let qAndA = [{
-        question: "How many primitive datatypes are there?",
-        choices: [
-            { text: '1',  correct: false},
-            { text: '2',  correct: false},
-            { text: '3',  correct: false}, 
-            { text: '4',  correct: true},
-    
-    ]  
-    },
-    {
-        question: "How do we notate values of an array?",
-        choices: [
-            { text: '{}',  correct: false},
-            { text: '[]',  correct: true},
-            { text: '()',  correct: false}, 
-            { text: '//',  correct: false},
-    
-    ],  
-    },
-    {
-        question: "How do we notate JQuery?",
-        choices: [
-            { text: '>',  correct: false},
-            { text: '<',  correct: false},
-            { text: '?',  correct: false}, 
-            { text: '$',  correct: true},
-    
-    ],  
-    },
-    {
-        question: "How many boolean values are there?",
-        choices: [
-            { text: '1',  correct: false},
-            { text: '2',  correct: true},
-            { text: '3',  correct: false}, 
-            { text: '4',  correct: false},
-    
-    ],  
-    },
-    ];
+    question: "How many primitive datatypes are there?",
+    choices: [
+        { text: '1', correct: false },
+        { text: '2', correct: false },
+        { text: '3', correct: false },
+        { text: '4', correct: true },
+
+    ]
+},
+{
+    question: "How do we notate values of an array?",
+    choices: [
+        { text: '{}', correct: false },
+        { text: '[]', correct: true },
+        { text: '()', correct: false },
+        { text: '//', correct: false },
+
+    ],
+},
+{
+    question: "How do we notate JQuery?",
+    choices: [
+        { text: '>', correct: false },
+        { text: '<', correct: false },
+        { text: '?', correct: false },
+        { text: '$', correct: true },
+
+    ],
+},
+{
+    question: "How many boolean values are there?",
+    choices: [
+        { text: '1', correct: false },
+        { text: '2', correct: true },
+        { text: '3', correct: false },
+        { text: '4', correct: false },
+
+    ],
+},
+];
 
 
 
-  let writtenQuestions = document.getElementById('written-questions');
-  let allChoicesButtons = document.getElementById('all-choices');
-  let nextButton = document.getElementById('next-button');
+let writtenQuestions = document.getElementById('written-questions');
+let allChoicesButtons = document.getElementById('all-choices');
+let nextButton = document.getElementById('next-button');
 
 
-  let currentQuestionIndex = 0;
-  let score = 0;
+let currentQuestionIndex = 0;
+let score = 0;
 
-  function startGame() {
+function startGame() {
     isWin = false;
     timerElement = 60;
     words.textContent = "";
@@ -71,8 +72,8 @@ let qAndA = [{
 };
 // I need to do the set wins and set losses next. When the timer runs out, game over still displays.
 
-function winGame(){
-    words.textContent= "You won";
+function winGame() {
+    words.textContent = "You won";
     winCounter++;
 }
 
@@ -84,83 +85,69 @@ function loseGame() {
 
 
 function startTimer() {
-  timerReset = setInterval(function() {
-    if (timerElement >= 1) {
-          timerElement--;
-    timerCount.textContent = timerElement;
-      if (isWin && timerElement > 0) {
-        clearInterval(timerReset);
-        winGame();
-      }
-    }
-    if (timerElement === 0) {
-      clearInterval(timerReset);
-      loseGame();
-      
-    }
-  }, 1000);
+    timerReset = setInterval(function () {
+        if (timerElement >= 1) {
+            timerElement--;
+            timerCount.textContent = timerElement;
+            if (isWin && timerElement > 0) {
+                clearInterval(timerReset);
+                winGame();
+            }
+        }
+        if (timerElement === 0) {
+            clearInterval(timerReset);
+            loseGame();
+
+        }
+    }, 1000);
 };
 
 
 
-  function startQuiz() {
+function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
     nextButton.textContent = 'Next';
     populateQuestion();
-  }
-   
-  function populateQuestion(){
+}
+
+function populateQuestion() {
     resetState();
     let currentQuestion = qAndA[currentQuestionIndex];
     let questionIndexNumber = currentQuestionIndex + 1;
     writtenQuestions.textContent = questionIndexNumber + ")" + currentQuestion.question;
 
-    currentQuestion.choices.forEach(choices=> {
+    currentQuestion.choices.forEach(choices => {
         let button = document.createElement("button");
         button.innerHTML = choices.text;
         button.classList.add('btn');
         allChoicesButtons.appendChild(button);
 
-        if(choices.correct){
+        if (choices.correct) {
             button.dataset.correct = choices.correct
         }
         button.addEventListener("click", selectChoice);
-    });       
+    });
 }
 
-function resetState(){
-    nextButton.style.display='none';
-    while(allChoicesButtons.firstChild){
+function resetState() {
+    nextButton.style.display = 'none';
+    while (allChoicesButtons.firstChild) {
         allChoicesButtons.removeChild(allChoicesButtons.firstChild);
     }
 }
 
-// function loseTime(){
-//     for (let index = 0; index < qAndA.length; index++) {
-        
-//         for (let j = 0; j < qAndA[j].choices.length; j++) {
-//             const element = qAndA[j].choices;
-            
-//             console.log(element)
-//         }
-
-//         if(element.correct === false) {
-//             timerElement --;
-//         }
-//     }
-// };
-
-// console.log(loseTime())
-
 function selectChoice(e) {
     let selectedBtn = e.target;
     let isCorrect = selectedBtn.dataset.correct === "true";
-    if(isCorrect){
+    if (isCorrect) {
         selectedBtn.classList.add('correct');
-        score ++;
+        resultElement.textContent = 'Correct!'
+        score++;
+
     } else {
         selectedBtn.classList.add('incorrect');
+        resultElement.textContent = 'Incorrect!'
         timerElement -= 5;
     }
     nextButton.style.display = "block";
@@ -170,35 +157,36 @@ function selectChoice(e) {
 
 
 
-function showScore(){
+function showScore() {
     resetState();
     writtenQuestions.textContent = ` You got ${score} out of ${qAndA.length} correct!`;
     nextButton.textContent = 'Play Again'
     nextButton.style.display = 'block'
+    clearInterval(timerReset);
 
 }
 
-function displayNextButton(){
+function displayNextButton() {
+  
     currentQuestionIndex++;
-    if(currentQuestionIndex < qAndA.length){
+    if (currentQuestionIndex < qAndA.length) {
         populateQuestion();
     } else {
         showScore();
     }
 }
- 
-console.log(populateQuestion)
+
 
 startButton.addEventListener("click", startGame);
 
 
-nextButton.addEventListener('click', ()=> {
-    if(currentQuestionIndex < qAndA.length){
+nextButton.addEventListener('click', () => {
+    if (currentQuestionIndex < qAndA.length) {
         displayNextButton();
     } else {
+        startGame();
         startQuiz();
     }
 });
 
 startQuiz();
-  
